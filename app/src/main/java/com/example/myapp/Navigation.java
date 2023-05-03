71fa0516a1148fc734792dcc6076ace9a8527a9f
TomerKidron1 <113578708+TomerKidron1@users.noreply.github.com>
2023-05-03 15:41:01 +0300

    3/5/23

---
+++ app/src/main/java/com/example/myapp/Navigation.java
@@ -0,0 +1,14 @@
+package com.example.myapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Navigation extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.navigation);
+    }
+}
